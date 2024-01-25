5b07593806780a2f75066b58ac31c0c987d5c7ce
code-with-ian-lgtm <nicholasian2304@gmail.com>
2024-01-25 11:11:28 +0300

    commit

---
+++ food_delivery_app/lib/theme/dark_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme:ColorScheme.dark(
+    background: Colors.black,
+    primary: Colors.grey.shade100,
+    secondary: Colors.grey.shade400,
+    tertiary: Colors.grey.shade700,
+    inversePrimary: Colors.grey.shade800
+  )
+);
