d9a2389171458247f591208ee1eee70b45f66540
code-with-ian-lgtm <nicholasian2304@gmail.com>
2024-01-25 10:57:11 +0300

    commit

---
+++ food_delivery_app/lib/theme/light_mode.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.grey,
+    primary: Colors.grey.shade300,
+    secondary: Colors.grey.shade700,
+    tertiary: Colors.grey.shade800,
+    inversePrimary: Colors.grey.shade500,
+
+  )
+);
